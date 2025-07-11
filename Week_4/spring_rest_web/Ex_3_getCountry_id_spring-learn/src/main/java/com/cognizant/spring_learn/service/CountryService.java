122320d5af4d77dc1955306e59d2994cde4f31bb
DhaariniD <dhaarini2104@gmail.com>
2025-07-11 06:20:11 +0530

    Solution for get country by id

---
+++ Week_4/spring_rest_web/Ex_3_getCountry_id_spring-learn/src/main/java/com/cognizant/spring_learn/service/CountryService.java
@@ -0,0 +1,27 @@
+package com.cognizant.spring_learn.service;
+
+import com.cognizant.spring_learn.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService{
+
+    private List<Country> countryList;
+
+    public CountryService() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        this.countryList = (List<Country>) context.getBean("countryList");
+    }
+
+    public Country getCountry(String code) {
+        return countryList.stream()
+                .filter(c -> c.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null);
+    }
+}
