f66b4d93af7944dbc97e9dbefbaf906a4a58c441
DhaariniD <dhaarini2104@gmail.com>
2025-07-25 14:31:37 +0530

    solution added

---
+++ Week_6/ex_5_cohorttracker/src/App.js
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import './App.css';
+import CohortDetails from './CohortDetails';
+
+const cohorts = [
+  {
+    name: "INTADMDF10 -.NET FSD",
+    status: "Scheduled",
+    startDate: "22-Feb-2022",
+    coach: "Aathma",
+    trainer: "Jojo Jose"
+  },
+  {
+    name: "ADM21JF014 - Java FSD",
+    status: "Ongoing",
+    startDate: "10-Sep-2021",
+    coach: "Apoorv",
+    trainer: "Elisa Smith"
+  },
+  {
+    name: "CDBJF21025 - Java FSD",
+    status: "Ongoing",
+    startDate: "24-Dec-2021",
+    coach: "Aathma",
+    trainer: "John Doe"
+  }
+];
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cohorts Details</h1>
+      {cohorts.map((cohort, index) => (
+        <CohortDetails key={index} cohort={cohort} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
